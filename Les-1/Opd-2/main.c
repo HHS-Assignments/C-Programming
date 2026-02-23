2386b7e761a3225ce1fa35252fafb45d7c47e322
The_LEGEND23 <tom.steenvoorden23@gmail.com>
2026-02-23 16:16:51 +0100

    Opdracht 1.4 Gedaan

--- Les-1/Opd-2/main.c
+++ Les-1/Opd-2/main.c
@@ -7,28 +7,22 @@ Verberg de complexiteit van het maken van een random getal in een functie */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 int randomGetal(int ondergrens, int bovengrens) {
     return ondergrens + rand() % (bovengrens - ondergrens + 1);
 }
-
 int main() {
     char nogmaals;
     srand(time(NULL));
-    
     do {
         int ondergrens, bovengrens, getal, gok;
         int gokken[100];
         int aantal = 0;
-        
         printf("\nGeef ondergrens: ");
         scanf("%d", &ondergrens);
         printf("Geef bovengrens: ");
         scanf("%d", &bovengrens);
-        
         getal = randomGetal(ondergrens, bovengrens);
         printf("\nRaad het getal tussen %d en %d!\n", ondergrens, bovengrens);
-        
         for (int i = 0; i < 7; i++) {
             printf("Gok %d: ", i + 1);
             scanf("%d", &gok);
@@ -44,21 +38,16 @@ int main() {
                 printf("Lager!\n");
             }
         }
-        
         if (gok != getal) {
             printf("Het getal was %d\n", getal);
         }
-        
         printf("Je gokken: ");
         for (int i = 0; i < aantal; i++) {
             printf("%d ", gokken[i]);
         }
         printf("\n");
-        
         printf("\nNogmaals spelen? (j/n): ");
         scanf(" %c", &nogmaals);
-        
     } while (nogmaals == 'j' || nogmaals == 'J');
-    
     return 0;
 }
--- Les-1/Opd-4/main.c
+++ Les-1/Opd-4/main.c
@@ -11,12 +11,94 @@ hetzelfde
 d) Uitdaging: Maak nu een variant waarbij het aantal worpen per beurt variabel is. Bijvoorbeeld
 een klein kind dat nog niet zo goed is mag 3x gooien. Maak de code wel zo generiek mogelijk
 en gebruik niet te veel if statements */
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 #include <stdbool.h>
 
-int main() {
+#define MAX_FRAMES 10
+#define MAX_WORPEN 21
 
+int bereken_score(int worpen[], int aantal) {
+    int score = 0, idx = 0;
+    for (int frame = 0; frame < MAX_FRAMES; frame++) {
+        if (worpen[idx] == 10) {
+            score += 10 + worpen[idx + 1] + worpen[idx + 2];
+            idx++;
+        } else if (worpen[idx] + worpen[idx + 1] == 10) {
+            score += 10 + worpen[idx + 2];
+            idx += 2;
+        } else {
+            score += worpen[idx] + worpen[idx + 1];
+            idx += 2;
+        }
+    }
+    return score;
+}
+
+int speel_spel(char* naam) {
+    int worpen[MAX_WORPEN] = {0}, idx = 0;
+    printf("\n=== %s ===\n", naam);
+    
+    for (int frame = 1; frame <= MAX_FRAMES; frame++) {
+        int pins = 10, max_worpen = (frame == 10) ? 3 : 2;
+        printf("\nFrame %d:\n", frame);
+        
+        for (int w = 1; w <= max_worpen; w++) {
+            int gepind;
+            while (scanf("%d", &gepind) != 1 || gepind < 0 || gepind > pins) {
+                printf("ERROR: voer 0-%d in: ", pins);
+                while (getchar() != '\n');
+            }
+            
+            printf("  Worp %d: %d pins\n", w, gepind);
+            worpen[idx++] = gepind;
+            pins -= gepind;
+            
+            if (gepind == 10 && w == 1 && frame < 10) {
+                printf("  STRIKE!\n");
+                break;
+            }
+            if (pins == 0 && w == 2 && frame < 10) {
+                printf("  SPARE!\n");
+                break;
+            }
+            if (frame == 10 && pins == 0) pins = 10;
+        }
+    }
+    return bereken_score(worpen, idx);
+}
+
+int main() {
+    printf("=== BOWLING ===\n");
+    
+    int spelers;
+    printf("Aantal spelers (1-2): ");
+    while (scanf("%d", &spelers) != 1 || spelers < 1 || spelers > 2) {
+        printf("ERROR: voer 1 of 2 in: ");
+        while (getchar() != '\n');
+    }
+    
+    char naam1[50], naam2[50];
+    int score1, score2 = 0;
+    
+    printf("Naam speler 1: ");
+    scanf("%s", naam1);
+    score1 = speel_spel(naam1);
+    
+    if (spelers == 2) {
+        printf("\nNaam speler 2: ");
+        scanf("%s", naam2);
+        score2 = speel_spel(naam2);
+    }
+    
+    printf("\n=== EINDSTAND ===\n");
+    printf("%s: %d punten\n", naam1, score1);
+    if (spelers == 2) {
+        printf("%s: %d punten\n", naam2, score2);
+        printf("Winnaar: %s\n", score1 > score2 ? naam1 : score2 > score1 ? naam2 : "Gelijkspel");
+    }
+    
     return 0;
 }
+
+// Testwaarden: Strike (10,3,4)=24 | Spare (7,3,4,2)=20 | Perfect game=300
